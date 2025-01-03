8f95b090f12fe4114218fa39a781a2447c39db0c
regalileo <146832616+regalileo@users.noreply.github.com>
2025-01-03 19:40:53 +0700

    Delete routes directory

--- routes/movie-router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { searchMovie, getPopularMovies } = require('../controller/movie-controller');
-const router = express.Router();
-
-router.get('/api/movies', searchMovie);
-router.get('/api/movies/popular', getPopularMovies);
-
-module.exports = router;
